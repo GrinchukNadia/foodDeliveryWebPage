320314548e0a5b84535967996e90043a426912d8
Надежда <grennad991@gmail.com>
2020-12-19 22:23:10 +0200

    Sales timer section. Footer.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+// Tabs Slider in header
 
     const slide = document.querySelectorAll('.slide_item'),
           slideImg = document.querySelectorAll('.slider_img'),
@@ -32,7 +33,48 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+// Timer
 
+    const deadline = '2021-05-12';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / 1000 / 60 / 60) % 24),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor( (t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+    // '.salle__timer'
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('.days'),
+              hours = timer.querySelector('.hours'),
+              minutes = timer.querySelector('.minutes'),
+              seconds = timer.querySelector('.seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = t.days;
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('.salle__timer', deadline);
 
 
 });
