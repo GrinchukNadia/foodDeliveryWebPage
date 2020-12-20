ff5497a290d1aa13c9277cb62e2a10d3a88cb9eb
Надежда <grennad991@gmail.com>
2020-12-20 22:01:41 +0200

    Modal window and functionality. Cards of menu and functionality to add them on page dinamicly

--- js/script.js
+++ js/script.js
@@ -33,6 +33,7 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
 // Timer
 
     const deadline = '2021-05-12';
@@ -52,7 +53,15 @@ window.addEventListener('DOMContentLoaded', () => {
             'seconds': seconds
         };
     }
-    // '.salle__timer'
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`
+        } else {
+            return num;
+        }
+    }
+    
     function setClock(selector, endtime) {
         const timer = document.querySelector(selector),
               days = timer.querySelector('.days'),
@@ -61,13 +70,15 @@ window.addEventListener('DOMContentLoaded', () => {
               seconds = timer.querySelector('.seconds'),
               timeInterval = setInterval(updateClock, 1000);
 
+        updateClock();
+
         function updateClock() {
             const t = getTimeRemaining(endtime);
 
-            days.innerHTML = t.days;
-            hours.innerHTML = t.hours;
-            minutes.innerHTML = t.minutes;
-            seconds.innerHTML = t.seconds;
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
@@ -75,6 +86,176 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
     setClock('.salle__timer', deadline);
+
+//Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalClose = document.querySelector('[data-close]');
+
+    modalTrigger.forEach( btn => {
+            btn.addEventListener('click', () => {
+            openModal();
+        });
+    });
+
+    function openModal(){
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalInterval);
+    }
+
+    function closeModal(){
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalClose.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target == modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code == "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    const modalInterval = setTimeout(openModal, 6000);
+    function showModalByScroll(){
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
+//Cards
+    class MenuItem {
+        constructor(img, alt, title, description, price, parentSelector, ...classes) {
+            this.img = img;
+            this.alt = alt;
+            this.title = title;
+            this.description = description;
+            this.price = price;
+            this.classes = classes;
+            this.parentSelector = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+        appendCards(){
+            const element = document.createElement('div');
+            if( this.classes.length === 0) {
+                this.element = 'card_menu';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+            element.innerHTML += `
+                <img src="img/${this.img}" alt="${this.alt}">
+                <div class="card_option card_title">${this.title}</div>
+                <div class="card_option card_description">${this.description}</div>
+                <div class="card_option card_divider"></div>
+                <div class="card_option card_price">
+                    <div class="inner_price">Цена:</div>
+                    <div class="inner_cost"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+            this.parentSelector.append(element);
+        }
+    }
+
+    const low = new MenuItem(
+        'menu1.jpg',
+        'Меню снижение веса',
+        'Снижение веса',
+        'Диета сбалансирована и эффективна, главный ее результат – изменений пищевых привычек человека и нормализация метаболизма. Меню на неделю поможет разобраться в данной системе питания.',
+        8,
+        '.menu_items-inner'
+    );
+    const medium = new MenuItem(
+        'menu2.jpg',
+        'Меню поддержки веса',
+        'Поддержка веса',
+        'В организации здорового питания главное — постепенность и правильный психологический настрой. Не стоит нацеливаться на жесткие ограничения и отказ от любимых продуктов.',
+        12,
+        '.menu_items-inner'
+    );
+    const hight = new MenuItem(
+        'menu3.jpg',
+        'Меню набора веса',
+        'Набор веса',
+        'Как ни странно, но некоторых людей мучает проблема низкого веса. Решение есть: нужно правильно подобрать меню для набора веса. Ведь человек с недостаточным весом никогда не будет хорошо выглядеть, и на здоровье это тоже плохо сказывается. ',
+        19,
+        '.menu_items-inner'
+    );
+
+        low.appendCards();
+        medium.appendCards();
+        hight.appendCards();
+
+//Server Forms
+
+        const forms = document.querySelectorAll('form');
+
+        const message = {
+            loading: 'Загрузка',
+            success: 'Спасибо! Скоро мы с вами свяжемся.',
+            failure: 'Что-то пошло не так'
+        };
+         
+        forms.forEach( item => {
+            postData(item);
+        });
+
+        function postData(form) {
+            form.addEventListener('submit', (e) => {
+                e.preventDefault();
+
+                const statusMessage = document.createElement('div');
+                statusMessage.classList.add('status');
+                statusMessage.textContent = message.loading;
+                form.append(statusMessage);
+
+                const request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+
+                request.setRequestHeader('Content-type', 'application/json');
+                const formData = new FormData(form);
+
+                const object = {};
+                formData.forEach( function(value, key){
+                    object[key] = value;
+                });
+
+                const json = JSON.stringify(object);
+
+                request.send(json);
+
+                request.addEventListener('load', () => {
+                    if (request.status === 200) {
+                        console.log(request.response);
+                        statusMessage.textContent = message.success;
+                        form.reset();
+                        setTimeout( () => {
+                            statusMessage.remove();
+                        }, 2000);
+                    } else {
+                        statusMessage.textContent = message.failure;
+                    }
+                });
+            });
+        }
+
 
 
 });
